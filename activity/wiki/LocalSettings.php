821bd5ff2106c5ce0ef365c9593b8a839515d168
frankchen <>
2019-03-04 13:42:55 +0800

    update

--- activity/wiki/LocalSettings.php
+++ activity/wiki/LocalSettings.php
@@ -18,8 +18,8 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 ## Uncomment this to disable output compression
 # $wgDisableOutputCompression = true;
 
-$wgSitename = "malichina";
-$wgMetaNamespace = "Malichina";
+$wgSitename = "youyoucode";
+$wgMetaNamespace = "youyoucode";
 
 ## The URL base path to the directory containing the wiki;
 ## defaults for all runtime URL paths are based off of this.
